66a4bb844747647a5d64ef8ce1c1642b782aeff6
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2025-01-15 21:13:00 +0530

    chore: Basic Assets

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home = () => {
+  return (
+    <SafeAreaView className="flex-1 w-screen items-center justify-center bg-white0">
+      <View className="">
+        <Text>Home</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
