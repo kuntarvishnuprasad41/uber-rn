44a969d6af8f306ee0572023408ae6f5cfa9359d
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2025-01-15 21:51:41 +0530

    swiper

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,11 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useRef } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+import Swiper from "react-native-swiper";
 
 const OnBoarding = () => {
+  const swiperRef = useRef<Swiper>(null);
   return (
-    <SafeAreaView>
-      <Text>OnBoarding</Text>
+    <SafeAreaView className="flex-1 items-center    w-screen ">
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(auth)/sign-up");
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={<View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0]"></View>}
+      ></Swiper>
     </SafeAreaView>
   );
 };
