0833f751a69ad2787f1d7ab2d32329098fae7a98
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2025-01-19 20:30:41 +0530

    Fix native

--- app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Redirect } from "expo-router";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-
-const Home = () => {
-  return <Redirect href={"/(auth)/welcome"} />;
-};
-
-export default Home;
