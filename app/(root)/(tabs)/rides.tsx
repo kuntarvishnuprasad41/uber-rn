cbdb215b615e769276f680917268454c0d5b8786
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2025-01-21 11:21:01 +0530

    Chore: Rename file

--- app/(root)/(tabs)/rides.tsx
+++ app/(root)/(tabs)/rides.tsx
@@ -1,12 +1,12 @@
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const History = () => {
+const Rides = () => {
   return (
     <SafeAreaView className="flex-1 items-center justify-center">
-      <Text>History</Text>
+      <Text>Rides</Text>
     </SafeAreaView>
   );
 };
 
-export default History;
+export default Rides;
